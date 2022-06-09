8010aa687d7455e286116d2f1a7dad4f34c70c3d
ywjdlm <1571754405@qq.com>
2022-06-09 15:23:35 +0800

    新增yaml文件的部分方法

---
+++ common/getnest_util.py
@@ -0,0 +1,24 @@
+﻿#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+class getnestutil():
+
+
+
+    def get_dic_nest(self, dic, objkey, default):
+        """
+        获取字典中的key对应的值，适用于字典嵌套
+        dic:已知的字典
+        key:已知的键--key
+        default:找不到时返回的默认值
+        """
+        dic1 = dic
+        for key, value in dic1.items():
+            if key == objkey:
+                return value
+
+            else:
+                if isinstance(value, dict):
+                    new_value = getnestutil().get_dic_nest(value, objkey, default)
+                    if new_value is not default:
+                        return new_value
+        return default
