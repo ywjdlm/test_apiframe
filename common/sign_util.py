0b2d8ade09edd04430e6332c7090785a849d9772
ywjdlm <1571754405@qq.com>
2022-06-14 11:34:05 +0800

    截图学习sign_util

--- common/sign_util.py
+++ common/sign_util.py
@@ -24,27 +24,19 @@ class signutil():
 
     #根据筛选后的传参数据生成MD5加密格式的字符串
     def SignString(self,data):
-
         signData = signutil().GetSignData(data)#调用GetSignData方法获取非空的参数名以及参数值,返回一个字典格式的数据
-
         #将筛选后的data的参数名列成列表
         list = signData.keys()
-
         #定义一个空字符串
         stringA = ''
-
         #拼接成参数名=参数值&....&参数名N=参数值N的格式
         for i in list:
-
             #判断数据的值是否是整数型,若为整数型则转换为字符串进行拼接
             if type(signData[i])==int:
                 signData[i]=str(signData[i])
-
             stringA = stringA + i + "=" + signData[i] + "&"
-
         #截取全新字符串,去掉"&"符号
         stringA = stringA[0:-1]
-
         #视情况,若需要增加sign_key则注释 stringA = stringA[0:-1]即可
         #stringA =stringA+ "sign_key" + "=" + "4ef7692d629fc3760ef432cb772b9ed0"
         return stringA
@@ -64,22 +56,22 @@ class signutil():
         return signValue
 
 
-# if __name__ == '__main__':
-#     alldata = {
-#         "content": "哦哦哦哦",
-#         "image": "http://static.rulin49.com/055ef6397ec8d2ef6308ee35acf4108b.jpg?w=1334&h=750 ",
-#         "labelId": "49",
-#         "locationId": "60a8c64c91a04c4d98ccf48ac51243d8",
-#         "sign": "00612E7A9FFDFF2C24284BC89E9A35DA",
-#         "type": "6",
-#         "bid":'null',
-#         "num":"",
-#         'phone':1571754405
-#     }
-#
-#     data=['sign','','null']
-#     print(signutil().SignString(alldata))
-#     print("==========================================================================================")
-#     a=signutil().getsign(alldata)
-#     print(a)
+if __name__ == '__main__':
+    alldata = {
+        "content": "哦哦哦哦",
+        "image": "http://static.rulin49.com/055ef6397ec8d2ef6308ee35acf4108b.jpg?w=1334&h=750 ",
+        "labelId": "49",
+        "locationId": "60a8c64c91a04c4d98ccf48ac51243d8",
+        "sign": "00612E7A9FFDFF2C24284BC89E9A35DA",
+        "type": "6",
+        "bid":'null',
+        "num":"",
+        'phone':1571754405
+    }
+
+    data=['sign','','null']
+    print(signutil().SignString(alldata))
+    print("==========================================================================================")
+    a=signutil().getsign(alldata)
+    print(a)
 
