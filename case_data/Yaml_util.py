545b3b7f7db11c1614f5128c2dead3c735494824
ywjdlm <1571754405@qq.com>
2022-06-17 15:21:57 +0800

    新增解析嵌套字典的方法

--- case_data/Yaml_util.py
+++ case_data/Yaml_util.py
@@ -88,7 +88,7 @@ class Yamlutil():
 
     def diguichaxun(self, yaml_file, objkey, ex_value=None, default=None, groupname=None,val=None):
         try:
-            data = Yamlutil().read_moreAPI_Yaml(yaml_file, groupname)
+            data = Yamlutil().read_moreAPI_Yaml(yaml_file, groupname=groupname)
         except:
             raise FileNotFoundError('输入的yaml文件格式错误!!!')
 
@@ -148,6 +148,7 @@ class Yamlutil():
     #通过键值对模糊查询所有的字典数据,返回一个列表结果
     def mohuselect_dic(self,yaml_file, objkey, ex_value, default=None, groupname=None):
         try:
+            #通过read_moreAPI_Yaml方法获取
             data = Yamlutil().read_moreAPI_Yaml(yaml_file, groupname)
         except:
             raise FileNotFoundError('输入的yaml文件格式错误!!!')
@@ -163,7 +164,7 @@ class Yamlutil():
             Exception('没有找到匹配的数剧,请查阅对应yaml文件!!!')
 
 
-
+    #通过值进行模糊查询
     """
     extract临时文件读写清空操作
     1.key为键值对格式或者键=值格式传参
@@ -186,69 +187,29 @@ class Yamlutil():
         with open(pathutil().get_path_project()+"\\case_data\\extract.yaml", mode="w", encoding="utf-8") as f:
             f.truncate()
 
-        # 利用Template方法,
-        def Yaml_replace(self, yaml_file, groupname, dic):
-            """
-            Template 中主要的俩种格式：
-            1：$variable 使用 $变量名 引用变量
-            2：${variable} 使用 ${变量名} 大括号包起来,在yaml文件里定义变量: ${变量名}
-            eg:创建yaml文件，将数据存放进去，当中设置 username 为${user} ，password为 $password
+    # 利用Template方法,
+    def Yaml_replace(self, yaml_file, groupname, dic):
+        """
+        Template 中主要的俩种格式：
+        1：$variable 使用 $变量名 引用变量
+        2：${variable} 使用 ${变量名} 大括号包起来,在yaml文件里定义变量: ${变量名}
+        eg:创建yaml文件，将数据存放进去，当中设置 username 为${user} ，password为 $password
 
-            """
-            # 打开yaml文件并读取
-            with open(os.getcwd() + "\\case_data\\cases\\"+yaml_file, mode='r', encoding='utf-8') as f:
-                read_res = f.read()
-                # 获取yaml文件中的对应格式变量
-                temp = Template(read_res)
-                try:
-                    # 将写入的数据传输到变量中,获取新的值
-                    c = temp.safe_substitute(dic)
-                    yaml_data = yaml.safe_load(c)
-                    case_API = yaml_data[groupname]
-                    return case_API
+        """
+        # 打开yaml文件并读取
+        with open(os.getcwd() + "\\case_data\\cases\\"+yaml_file, mode='r', encoding='utf-8') as f:
+            read_res = f.read()
+            # 获取yaml文件中的对应格式变量
+            temp = Template(read_res)
+            try:
+                # 将写入的数据传输到变量中,获取新的值
+                c = temp.safe_substitute(dic)
+                yaml_data = yaml.safe_load(c)
+                case_API = yaml_data[groupname]
+                return case_API
+            except:
+                raise FileNotFoundError("需要替换的数据格式不为字典格式!!!")
 
-                except:
-                    raise FileNotFoundError("需要替换的数据格式不为字典格式!!!")
 
-if __name__ == '__main__':
 
-      data = {"code": 0, "data": {
-    "area": {"1": "东湖风景区", "10": "东西湖区", "11": "武汉经济技术开发区（汉南）", "12": "蔡甸区", "13": "江夏区", "14": "黄陂区", "15": "新洲区",
-             "2": "东湖新技术开发区", "3": "江岸区", "4": "江汉区", "5": "硚口区", "6": "汉阳区", "7": "武昌区", "8": "青山区", "9": "洪山区"},
-    "companytype": {"1": "全部", "2": "企业", "3": "个人组织", "4": "国家单位", "5": "事业单位"},
-    "education": {"1": "全部", "2": "高中", "3": "大专", "4": "本科", "5": "研究生", "6": "博士及以上"},
-    "maritalStatus": {"1": "未婚", "2": "已婚"},
-    "userInfo": {"id": 129, "createdAt": "2022-05-31T14:17:53+08:00", "updatedAt": "2022-06-12T18:02:20+08:00",
-                 "username": "", "openid": "", "nickname": "我爱我家我爱我家我",
-                 "avatar": "static/uploads/20220601/4fd19ebcbb226282c258b2022e99faec.jpg", "age": 36, "gender": 1,
-                 "height": 220, "danweiType": 5, "authDanwei": 2, "authReal": 2, "birthday": "1986/06/01",
-                 "occupation": "烘焙师",
-                 "photo": "static/uploads/20220601/8ab9659360e7c699adbd1d9720e7bb14.jpg,static/uploads/20220601/a29fa6354e3ee2835edb49945e077f76.jpg,static/uploads/20220601/395a2eed55611a9a34ca32a6f4bba54f.jpg,static/uploads/20220601/9c7bda0f34e37173e4e565082517b9e2.jpg",
-                 "education": 5, "live": "唐山", "about": "大家好，我是来自海洋，家住珊瑚中，朋友大多是小虾，我很期待遇见我的另一个她，哈哈哈哈哈",
-                 "desire": "期望遇见善良，稳重，传统的女孩子，这是我的理想型，小胖子，哈哈哈", "wxCode": "hahahah", "isLike": 0, "mutualLike": 0,
-                 "isLooking": 0, "likeMinheight": 120, "likeMaxheight": 220, "likeMinAge": 18, "likeMaxage": 45,
-                 "likeDanweiType": 1, "likeEducation": 1, "likeOccupation": "",
-                 "photos": ["static/uploads/20220601/8ab9659360e7c699adbd1d9720e7bb14.jpg",
-                            "static/uploads/20220601/a29fa6354e3ee2835edb49945e077f76.jpg",
-                            "static/uploads/20220601/395a2eed55611a9a34ca32a6f4bba54f.jpg",
-                            "static/uploads/20220601/9c7bda0f34e37173e4e565082517b9e2.jpg"], "tagList": [
-            {"id": 246, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-05-31T16:11:56+08:00", "uid": 129,
-             "tagName": "运动"},
-            {"id": 247, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-0 5-31T16:11:56+08:00", "uid": 129,
-             "tagName": "服饰"},
-            {"id": 248, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-05-31T16:11:56+08:00", "uid": 129,
-             "tagName": "打游戏"},
-            {"id": 249, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-05-31T16:11:56+08:00", "uid": 129,
-             "tagName": "阅读"},
-            {"id": 250, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-05-31T16:11:56+08:00", "uid": 129,
-             "tagName": "音乐"}], "educationExtend": "研究生", "area": 0, "maritalStatus": 1, "status": 1}}, "msg": "操作成功"}
-      # a = Yamlutil().get_dic_nest(data, 'wxCode')
-      # print(a)
-      # b=Yamlutil().diguichaxun('yongli.yml','assert','成功',groupname='login')
-      # print(b)
-      #
-      # c=Yamlutil().diguichaxun('yongli.yml','heads',val='1',groupname='login')
-      # print(c)
 
-      d=Yamlutil().mohuselect_dic('yongli.yml','assert',ex_value='失败',groupname='login')
-      print(d)
---
+++ common/nest_select_util.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+from case_data.Yaml_util import Yamlutil
+
+
+class nestselectutil():
+
+    # 嵌套字典分解后对比
+    def get_dic_nest(self, dic, objkey=None, default=None, values=None):
+        # 此分支使用键进行模糊查询
+        if values == None and objkey is not None:
+            try:
+                #获取所有的键
+                for key in dic.keys():
+                    if key == objkey:
+                        return key
+                    else:
+                        if isinstance(dic[key], dict):
+                            new_key = nestselectutil().get_dic_nest(dic[key], objkey=objkey, default=default, values=values)
+                            if new_key is not default:
+                                return new_key
+                return default
+            except:
+                raise FileNotFoundError('数据格式有误,无法执行,请检查数据格式!!!')
+
+        elif objkey == None and values is not None:
+            try:
+                for k, v in dic.items():
+                    if v == values:
+                        return v
+                    else:
+                        if isinstance(v, dict):
+                            new_v = nestselectutil().get_dic_nest(v,values=values)
+                            if new_v is not default:
+                                return new_v
+                return default
+            except:
+                raise FileNotFoundError('数据格式有误,无法执行,请检查数据格式!!!')
+
+        else:
+            """
+                   获取字典中的key对应的值，适用于字典嵌套
+                   dic:已知的字典
+                   objkey:已知的键--key
+                   default:找不到时返回的默认值
+                   """
+            try:
+                dic1 = dic
+                # 获取字典的键和值,判断字典的键是否等于已知的键
+                for key, value in dic1.items():
+                    if key == objkey:
+                        if value == values:
+                            return value
+                    else:
+                        # 判断值是否为字典,如果为字典则再判断一次,直到得到相同的键的值为止
+                        if isinstance(value, dict):
+                            new_value = nestselectutil().get_dic_nest(value, objkey=objkey,values=values, default=default)
+                            # 如果找到值就返回新值,没找到就返回默认
+                            if new_value is not default:
+                                return new_value
+                return default
+            except:
+                raise FileNotFoundError('数据格式有误,无法执行,请检查数据格式!!!')
+
+    def diguichaxun(self, lis_data=None,yaml_files=None, key=None, ex_value=None, default=None, groupnames=None):
+        if yaml_files is not None:
+            try:
+                # 获取yaml文件的值,即列表
+                data = Yamlutil().read_moreAPI_Yaml(yaml_file=yaml_files, groupname=groupnames)
+            except:
+                raise FileNotFoundError('输入的yaml文件格式错误!!!')
+        else:
+            data=lis_data
+
+
+        # 此分支用于使用键进行模糊查询,ex_value用于分支走向的判断
+        if ex_value == None:
+            lis = []
+            try:
+                # read_moreAPI_Yaml返回的是一个列表格式
+                # 先将列表拆成一个个字典
+                for i in data:
+                    # 如果val不为None,则走get_dic_nest里的这个if values is not None分支,如果查询到就返回查到的键
+                    a = nestselectutil().get_dic_nest(i,objkey=key)
+                    if key == a:
+                        lis.append(i)
+                return lis
+            except:
+                raise Exception("数据格式解析失败,请检查传入参数格式!!!!")
+
+        #此分支用于通过值模糊查询字典
+        elif key==None:
+            li=[]
+            try:
+                for i in data:
+                    a = nestselectutil().get_dic_nest(i, values=ex_value)
+                    if isinstance(str(a), str):
+                        if a == ex_value:
+                            li.append(i)
+                return li
+            except:
+                Exception('没有找到匹配的数剧,请查阅对应yaml文件!!!')
+
+
+
+        # 此分支用于精确查询,ex_value不能为空,判断查询到的键值是否是我想要的
+        else:
+            # read_get_test_yml返回的是一个列表格式
+            # 先将列表拆成一个个字典
+            if ex_value is not None:
+                for i in data:
+                    try:
+                        # 如果ex_value有值,则走向get_dic_nest里的else方法,判断值的类型,
+                        # 从而对比返回的值是否正确,若正确则返回一条精准数据,适用于精准查询,有且只有一条数据
+                        a = nestselectutil().get_dic_nest(i, objkey=key,values=ex_value,default=default)
+                        if isinstance(a, str):
+                            if ex_value == a:
+                                return i
+                        elif isinstance(a, int):
+                            if ex_value == a:
+                                return i
+                        else:
+                            if a == None:
+                                pass
+                            else:
+                                if ex_value in a.values():
+                                    return i
+                    except:
+                        raise FileNotFoundError('没有找到匹配的数剧,请查阅对应yaml文件!!!')
+            else:
+                raise Exception('方法中ex_value未填写,无法判断!!!')
+
+    # h获取嵌套字典的已知键对应的值
+    def get_dic_nest_val(self, dic, objkeys, default=None):
+        """
+        获取字典中的key对应的值，适用于字典嵌套
+        dic:已知的字典
+        key:已知的键--key
+        default:找不到时返回的默认值
+        """
+        try:
+            dic1 = dic
+            for key, value in dic1.items():
+                if key == objkeys:
+                    return value
+                else:
+                    if isinstance(value, dict):
+                        new_value = Yamlutil().get_dic_nest(value, objkeys, default)
+                        if new_value is not default:
+                            return new_value
+            return default
+        except:
+            raise FileNotFoundError('数据格式有误,无法执行,请检查数据格式!!!')
+
+
+if __name__ == '__main__':
+    # 测试精确查询,已知key和值的时候
+    a = nestselectutil().diguichaxun(yaml_files='yongli.yml', key='x-token',ex_value=123,groupnames='login')
+    print(a)
+
+    # 测试已知值ex_value,查用例
+    A = nestselectutil().diguichaxun(yaml_files='yongli.yml', ex_value='失败',groupnames='login')
+    print(A)
+
+    #测试已知键key查用例
+    B = nestselectutil().diguichaxun(yaml_files='yongli.yml', key='mobile',groupnames='getcode')
+    print(B)
+
+    # 测试get_dic_nest_val和diguichaxun的else分支data=lis_data的走向,
+    # data = {"code": 0, "data": {
+    #     "area": {"1": "东湖风景区", "10": "东西湖区", "11": "武汉经济技术开发区（汉南）", "12": "蔡甸区", "13": "江夏区", "14": "黄陂区", "15": "新洲区",
+    #              "2": "东湖新技术开发区", "3": "江岸区", "4": "江汉区", "5": "硚口区", "6": "汉阳区", "7": "武昌区", "8": "青山区", "9": "洪山区"},
+    #     "companytype": {"1": "全部", "2": "企业", "3": "个人组织", "4": "国家单位", "5": "事业单位"},
+    #     "education": {"1": "全部", "2": "高中", "3": "大专", "4": "本科", "5": "研究生", "6": "博士及以上"},
+    #     "maritalStatus": {"1": "未婚", "2": "已婚"},
+    #     "userInfo": {"id": 129, "createdAt": "2022-05-31T14:17:53+08:00", "updatedAt": "2022-06-12T18:02:20+08:00",
+    #                  "username": "", "openid": "", "nickname": "我爱我家我爱我家我",
+    #                  "avatar": "static/uploads/20220601/4fd19ebcbb226282c258b2022e99faec.jpg", "age": 36, "gender": 1,
+    #                  "height": 220, "danweiType": 5, "authDanwei": 2, "authReal": 2, "birthday": "1986/06/01",
+    #                  "occupation": "烘焙师",
+    #                  "photo": "static/uploads/20220601/8ab9659360e7c699adbd1d9720e7bb14.jpg,static/uploads/20220601/a29fa6354e3ee2835edb49945e077f76.jpg,static/uploads/20220601/395a2eed55611a9a34ca32a6f4bba54f.jpg,static/uploads/20220601/9c7bda0f34e37173e4e565082517b9e2.jpg",
+    #                  "education": 5, "live": "唐山", "about": "大家好，我是来自海洋，家住珊瑚中，朋友大多是小虾，我很期待遇见我的另一个她，哈哈哈哈哈",
+    #                  "desire": "期望遇见善良，稳重，传统的女孩子，这是我的理想型，小胖子，哈哈哈", "wxCode": "hahahah", "isLike": 0, "mutualLike": 0,
+    #                  "isLooking": 0, "likeMinheight": 120, "likeMaxheight": 220, "likeMinAge": 18, "likeMaxage": 45,
+    #                  "likeDanweiType": 1, "likeEducation": 1, "likeOccupation": "",
+    #                  "photos": ["static/uploads/20220601/8ab9659360e7c699adbd1d9720e7bb14.jpg",
+    #                             "static/uploads/20220601/a29fa6354e3ee2835edb49945e077f76.jpg",
+    #                             "static/uploads/20220601/395a2eed55611a9a34ca32a6f4bba54f.jpg",
+    #                             "static/uploads/20220601/9c7bda0f34e37173e4e565082517b9e2.jpg"], "tagList": [
+    #             {"id": 246, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-05-31T16:11:56+08:00",
+    #              "uid": 129,
+    #              "tagName": "运动"},
+    #             {"id": 247, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-0 5-31T16:11:56+08:00",
+    #              "uid": 129,
+    #              "tagName": "服饰"},
+    #             {"id": 248, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-05-31T16:11:56+08:00",
+    #              "uid": 129,
+    #              "tagName": "打游戏"},
+    #             {"id": 249, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-05-31T16:11:56+08:00",
+    #              "uid": 129,
+    #              "tagName": "阅读"},
+    #             {"id": 250, "createdAt": "2022-05-31T16:11:56+08:00", "updatedAt": "2022-05-31T16:11:56+08:00",
+    #              "uid": 129,
+    #              "tagName": "音乐"}], "educationExtend": "研究生", "area": 0, "maritalStatus": 1, "status": 1}},
+    #         "msg": "操作成功"}
+    # b=nestselectutil().get_dic_nest_val(data,objkeys='userInfo')
+    # c=nestselectutil().diguichaxun(lis_data=b['tagList'],ex_value=250)
+    # print(c)
